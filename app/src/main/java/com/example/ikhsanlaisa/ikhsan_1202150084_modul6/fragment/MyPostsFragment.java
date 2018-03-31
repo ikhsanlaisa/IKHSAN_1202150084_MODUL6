7f6c829c9717dfadd5ef93fed2b28e053b1a2489
ikhsan laisa <ikhsanlaisa96@gmail.com>
2018-04-01 05:13:47 +0700

    selesai

---
+++ app/src/main/java/com/example/ikhsanlaisa/ikhsan_1202150084_modul6/fragment/MyPostsFragment.java
@@ -0,0 +1,27 @@
+package com.example.ikhsanlaisa.ikhsan_1202150084_modul6.fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+/**
+ * Created by Umam on 3/31/2018.
+ */
+
+public class MyPostsFragment extends PostListFragment {
+
+    public MyPostsFragment() {}
+
+
+
+    @Override
+
+    public Query getQuery(DatabaseReference databaseReference) {
+
+        // All my posts
+
+        return databaseReference.child("user-posts")
+
+                .child(getUid());
+
+    }
+}
